4fb6bc0c2854bacf86881044b06cb2fbdd051f06
ministrov <61511030+ministrov@users.noreply.github.com>
2023-10-21 17:52:49 +0300

    feat: keep on operating on modal open

--- src/js/components/test.js
+++ src/js/components/test.js
@@ -1,6 +1,7 @@
 const itemButton = document.querySelector('.table-wrapper-btn');
-const modal = document.querySelector('.modal-overlay');
+const modal = document.querySelector('.modal');
 
 itemButton.addEventListener('click', () => {
   modal.classList.toggle('modal--open');
+  document.body.classList.toggle('overlay');
 });
