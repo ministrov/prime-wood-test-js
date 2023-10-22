f307191b4c8481f255e42fd1760ded6c3b191173
ministrov <61511030+ministrov@users.noreply.github.com>
2023-10-22 16:08:22 +0300

    feat: end up operating with opening modal popup

--- src/js/components/test.js
+++
@@ -1,7 +0,0 @@
-const itemButton = document.querySelector('.table-wrapper-btn');
-const modal = document.querySelector('.modal');
-
-itemButton.addEventListener('click', () => {
-  modal.classList.toggle('modal--open');
-  document.body.classList.toggle('overlay');
-});
